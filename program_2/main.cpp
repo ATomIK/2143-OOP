5fefd8ba4e6b7c2e0c2edf463506cf67b9341fe7
UT-Tom <tom@thomasj.me>
2017-09-26 16:14:36 -0500

    main alg

---
+++ program_2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	// generate random asteroids of size x
+
+	// launch ship
+
+	// loop x times
+
+		// find target
+
+		// mine asteroid
+				// asteroid > ship ? explode
+				// else collect()
+
+	// endloop
+
+
+
+	return 0;
+}
